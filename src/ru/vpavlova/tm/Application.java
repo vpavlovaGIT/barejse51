d865382f7203030c7b19dc704094785cee6f7d33
Victoria Pavlova <vpavlova@tsconsulting.ru>
2020-12-11 11:10:15 +0300

    Внесены изменения в проект.

--- src/ru/vpavlova/tm/Application.java
+++ src/ru/vpavlova/tm/Application.java
@@ -3,6 +3,7 @@ package ru.vpavlova.tm;
 public class Application {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
+
 }
