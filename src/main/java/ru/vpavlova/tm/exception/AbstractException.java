e264dfc9a3900555a730a7f924d576f8e0571555
Victoria Pavlova <vpavlova@tsconsulting.ru>
2021-05-04 12:11:11 +0300

    Удаление неактуальных папок.

--- src/main/java/ru/vpavlova/tm/exception/AbstractException.java
+++
@@ -1,9 +0,0 @@
-package ru.vpavlova.tm.exception;
-
-public abstract class AbstractException extends RuntimeException {
-
-    public AbstractException(String message) {
-        super(message);
-    }
-
-}
