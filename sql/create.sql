c0152a9e964fb2c9bafff0b92789bcb2de580a00
Victoria Pavlova <vpavlova@tsconsulting.ru>
2021-06-01 12:16:02 +0300

    Скорректирована кодировка.

--- sql/create.sql
+++ sql/create.sql
@@ -1,45 +1,45 @@
 CREATE TABLE `app_project` (
-	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
-	`name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`description` TEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`description` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	`dateStart` DATETIME NULL DEFAULT NULL,
 	`dateFinish` DATETIME NULL DEFAULT NULL,
-	`userId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`userId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='latin1_swedish_ci' ENGINE=InnoDB;
+COLLATE='utf8_general_ci' ENGINE=InnoDB;
 
 CREATE TABLE `app_session` (
-	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
 	`timestamp` BIGINT(20) NULL DEFAULT NULL,
-	`signature` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`userId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`signature` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`userId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='latin1_swedish_ci' ENGINE=InnoDB;
+COLLATE='utf8_general_ci' ENGINE=InnoDB;
 
 CREATE TABLE `app_task` (
-	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
-	`name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`description` TEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`description` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	`dateStart` DATETIME NULL DEFAULT NULL,
 	`dateFinish` DATETIME NULL DEFAULT NULL,
-	`projectId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`userId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`projectId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`userId` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='latin1_swedish_ci' ENGINE=InnoDB;
+COLLATE='utf8_general_ci' ENGINE=InnoDB;
 
 CREATE TABLE `app_user` (
-	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
-	`login` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`passwordHash` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`role` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`email` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`id` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`login` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`passwordHash` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`role` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	`locked` TINYINT(1) NULL DEFAULT NULL,
-	`firstName` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`lastName` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	`midName` VARCHAR(255) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`firstName` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`lastName` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`middleName` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
-COLLATE='latin1_swedish_ci' ENGINE=InnoDB;
+COLLATE='utf8_general_ci' ENGINE=InnoDB;
