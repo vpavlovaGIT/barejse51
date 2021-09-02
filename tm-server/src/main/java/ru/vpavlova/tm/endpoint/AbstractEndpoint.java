e567d90f6405250f391934ea4bcea390efe2a7d7
Victoria Pavlova <vpavlova@tsconsulting.ru>
2021-09-02 09:31:30 +0300

    Рефакторинг.

--- tm-server/src/main/java/ru/vpavlova/tm/endpoint/AbstractEndpoint.java
+++ tm-server/src/main/java/ru/vpavlova/tm/endpoint/AbstractEndpoint.java
@@ -8,7 +8,7 @@ public class AbstractEndpoint {
     @NotNull
     final ServiceLocator serviceLocator;
 
-    public AbstractEndpoint( ServiceLocator serviceLocator) {
+    public AbstractEndpoint(ServiceLocator serviceLocator) {
         this.serviceLocator = serviceLocator;
     }
 
